cddecfc0b0892831f93c01e6cf8c47132677caee
Masaya Kinefuchi <kine0718@icloud.com>
2023-05-05 21:05:15 +0900

    色素成分分離関数変更

--- RGB_pulse/program/_skinColorSeparation/program/pulseExtract_icexpress_hem.py
+++ RGB_pulse/program/_skinColorSeparation/program/pulseExtract_icexpress_hem.py
@@ -7,16 +7,16 @@ import sys
 import csv
 import re
 import os
-import funcSkinSeparation as ss
+import funcSkinSeparation2 as ss
 
 def getVideoROI(img):
     roi = cv2.selectROI(img)
     cv2.destroyAllWindows()
     return roi
 
-INPUT_DIR ='/Users/masayakinefuchi/脈波推定/exp-sub/ayumu/Cam 1/'
+INPUT_DIR ='/Users/masayakinefuchi/脈波推定/exp-sub/murai/Cam 1/'
 OUTPUT_DIR ='/Users/masayakinefuchi/脈波推定/imageSensing/RGB_pulse/program/_skinColorSeparation/result/'
-OUTPUT_FILE = OUTPUT_DIR +'ayumu' +'.csv'
+OUTPUT_FILE = OUTPUT_DIR +'murai1' +'.csv'
 
 
 
@@ -30,8 +30,11 @@ img = cv2.imread(img_name)
 width = int(img.shape[1])
 height = int(img.shape[0])
 
-roi = getVideoROI(img)
+selected_roi = getVideoROI(img)
+print(selected_roi)
 
+roi = [selected_roi[0],selected_roi[1],selected_roi[0]+100,selected_roi[1]+40]
+print(roi)
 pulsewave = np.zeros(int(num))
 time = np.zeros(int(num))
 
